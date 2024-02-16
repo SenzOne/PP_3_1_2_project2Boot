5c524a8162d15ce886b19d51b5f84352b3943f4a
Senz One <sem63sam@yandex.ru>
2024-02-16 13:27:03 +0400

    add ReadME.md and fix User controller

--- src/main/java/ru/senz0ne/springBootCRUD/project2Boot/controllers/UserController.java
+++ src/main/java/ru/senz0ne/springBootCRUD/project2Boot/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import ru.senz0ne.springBootCRUD.project2Boot.models.User;
 import ru.senz0ne.springBootCRUD.project2Boot.service.UserService;
 
